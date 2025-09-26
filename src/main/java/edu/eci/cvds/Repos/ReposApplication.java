b9413fa1f881c1e1cf6fb6d0f416e65fb4ea2900
unknown <carlos.avellaneda-f@labinfo.is.escuelaing.edu.co>
2025-09-26 13:30:31 -0500

    First Commit

---
+++ src/main/java/edu/eci/cvds/Repos/ReposApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.Repos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/cvds/Repos/ReposApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.Repos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
